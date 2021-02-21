23b563228a144a699e68bd37bcdd1a74e717acf0
涵曦 <hanxi.info@gmail.com>
2021-02-21 11:35:41 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/hanxi/nav',
 
   // 部署分支
   branch: 'main',
@@ -11,32 +11,32 @@ const c: IConfig = {
   hashMode: true,
 
   // 是否显示右上角的 Github 图标
-  showGithub: true,
+  showGithub: false,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https://nav.hanxi.info',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '私人导航 - 涵曦的私人书签分享',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '私人导航 - 涵曦的私人书签分享',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '导航,书签',
 
   // 默认主题: Light | Sim | Side | App
-  theme: 'Light',
+  theme: 'Side',
 
   // 网站底部内容, 版权信息、备案号, 可以是 HTML
   footerContent: `
     <div style="font-weight: bold;">共收录\${total}个网站</div>
-    <div>Copyright © 2018-2021 nav3.cn, All Rights Reserved</div>
+    <div>Copyright © 2018-2021 nav.hanxi.info, All Rights Reserved</div>
   `,
 
   // 百度统计地址
   // https://tongji.baidu.com/web/welcome/login
-  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?4582be7af7e7c95ef75351e07c6c32ba',
+  baiduStatisticsUrl: 'https://hm.baidu.com/hm.js?0032182cb564f380132d5bbcc48126c8',
 
   // CNZZ 统计地址
   // https://www.cnzz.com/o_index.php
